3c42706fb063a8f39c36e4e36b9face3566e17ab
Matthew James Briggs <matthew.james.briggs@gmail.com>
2023-07-07 13:53:08 -0700

    removing buildsys build

--- twoliter/build.rs
+++ twoliter/build.rs
@@ -1,42 +1,45 @@
-// use flate2::write::GzEncoder;
-// use flate2::Compression;
-// use std::fs::File;
-// use std::path::PathBuf;
-// use std::process::Command;
-// use std::{env, fs};
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+use std::path::PathBuf;
+use std::process::Command;
+use std::{env, fs};
 
-// const BOTTLEROCKET_PATH: &str = "./.ignore/hack/bottlerocket";
-//
-fn main() {}
-//     // checkout_bottlerocket();
-//     let bottlerocket_dir = PathBuf::from(BOTTLEROCKET_PATH);
-//
-//     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-//     let tools_dir = out_dir.join("tools");
-//     fs::create_dir_all(&tools_dir).unwrap();
-//     let tar_path = out_dir.join("tools.tar.gz");
-//
-//     // TODO - move this to the Twoliter git repo
-//     // Copy Bottlerocket Makefile.toml
-//     fs::copy(
-//         bottlerocket_dir.join("Makefile.toml"),
-//         tools_dir.join("Makefile.toml"),
-//     )
-//     .unwrap();
-//
-//     // Copy built Rust binary artifacts.
-//     // fs::copy(tools_dir.join(buildsys), tar_dir.join("buildsys")).unwrap();
-//     let buildsys_artifact = PathBuf::from(env::var("CARGO_BIN_FILE_BUILDSYS_buildsys").unwrap())
-//         .canonicalize()
-//         .unwrap();
-//     fs::copy(&buildsys_artifact, tools_dir.join("buildsys")).unwrap();
-//
-//     // Create tarball
-//     let tar_gz = File::create(&tar_path).unwrap();
-//     let enc = GzEncoder::new(&tar_gz, Compression::default());
-//     let mut tar = tar::Builder::new(enc);
-//     tar.append_dir_all("", &tools_dir).unwrap();
-// }
+const EMBED_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/embed");
+
+fn main() {
+    // checkout_bottlerocket();
+    let embed_dir = PathBuf::from(EMBED_DIR);
+    let embed_dir = embed_dir
+        .canonicalize()
+        .expect(&format!("Unable to canonicalize '{}'", embed_dir.display()));
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let tools_dir = out_dir.join("tools");
+    fs::create_dir_all(&tools_dir).unwrap();
+    let tar_path = out_dir.join("tools.tar.gz");
+
+    // TODO - move this to the Twoliter git repo
+    // Copy Bottlerocket Makefile.toml
+    fs::copy(
+        embed_dir.join("Makefile.toml"),
+        tools_dir.join("Makefile.toml"),
+    )
+    .unwrap();
+
+    // Copy built Rust binary artifacts.
+    // fs::copy(tools_dir.join(buildsys), tar_dir.join("buildsys")).unwrap();
+    let buildsys_artifact = PathBuf::from(env::var("CARGO_BIN_FILE_BUILDSYS_buildsys").unwrap())
+        .canonicalize()
+        .unwrap();
+    fs::copy(&buildsys_artifact, tools_dir.join("buildsys")).unwrap();
+
+    // Create tarball
+    let tar_gz = File::create(&tar_path).unwrap();
+    let enc = GzEncoder::new(&tar_gz, Compression::default());
+    let mut tar = tar::Builder::new(enc);
+    tar.append_dir_all("", &tools_dir).unwrap();
+}
 
 // /// This is a temporary function that will be removed once Bottlerocket tools are checked-in to this
 // /// repository.
