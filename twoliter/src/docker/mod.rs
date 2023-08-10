0b011dfc8b5c3d406f86d9ead568aac73892ef75
Matthew James Briggs <matthew.james.briggs@gmail.com>
2023-08-10 14:51:25 -0700

    mod cleanup

--- twoliter/src/docker/mod.rs
+++ twoliter/src/docker/mod.rs
@@ -1,9 +1,11 @@
 mod commands;
 mod image;
 mod twoliter;
+
 pub(crate) use self::commands::DockerBuild;
 pub(crate) use self::image::{ImageArchUri, ImageUri};
 pub(crate) use self::twoliter::create_twoliter_image_if_not_exists;
+
 pub(super) const DEFAULT_REGISTRY: &str = "public.ecr.aws/bottlerocket";
 pub(super) const DEFAULT_SDK_NAME: &str = "bottlerocket-sdk";
 // TODO - get this from lock file: https://github.com/bottlerocket-os/twoliter/issues/11
